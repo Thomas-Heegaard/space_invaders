0724ca24accea86be10cc257a0f6ced92a2882a5
Thomas Heegaard <thomas.h.heegaard@gmail.com>
2016-01-20 20:26:10 +0100

    Finalement on utilise des characteres spéciaux plustot que des groupes de characteres pour les personnages

--- 80C51/block.h
+++
@@ -1,8 +0,0 @@
-#ifndef BLOCK_H
-#define BLOCK_H
-
-#define ____BLK 0
-#define PLY_BLK 1
-#define ENM_BLK 2
-
-#endif //BLOCK_H
--- 80C51/settings.h
+++ 80C51/settings.h
@@ -8,12 +8,5 @@
 
 #define MAX_ENEMIES 5
 
-#define PLAYER {____BLK, PLY_BLK, ____BLK,\
-                PLY_BLK, PLY_BLK, PLY_BLK,\
-                PLY_BLK, ____BLK, PLY_BLK}
-
-#define ENEMY1 {ENM_BLK, ____BLK, ENM_BLK,\
-                ENM_BLK, ENM_BLK, ENM_BLK,\
-                ENM_BLK, ____BLK, ENM_BLK}
 
 #endif //SETTINGS_H
