0c6ba205885f2fa2c7191cbfe8d8f182a4c90e2b
Thomas Heegaard <thomas.h.heegaard@gmail.com>
2016-01-20 20:06:40 +0100

    added settings file

---
+++ 80C51/block.h
@@ -0,0 +1,8 @@
+#ifndef BLOCK_H
+#define BLOCK_H
+
+#define ____BLK 0
+#define PLY_BLK 1
+#define ENM_BLK 2
+
+#endif //BLOCK_H
---
+++ 80C51/settings.h
@@ -0,0 +1,19 @@
+#ifndef SETTINGS_H
+#define SETTINGS_H
+
+#include "block.h"
+
+#define SCENE_HEIGHT 20
+#define SCENE_WIDTH 20
+
+#define MAX_ENEMIES 5
+
+#define PLAYER {____BLK, PLY_BLK, ____BLK,\
+                PLY_BLK, PLY_BLK, PLY_BLK,\
+                PLY_BLK, ____BLK, PLY_BLK}
+
+#define ENEMY1 {ENM_BLK, ____BLK, ENM_BLK,\
+                ENM_BLK, ENM_BLK, ENM_BLK,\
+                ENM_BLK, ____BLK, ENM_BLK}
+
+#endif //SETTINGS_H
